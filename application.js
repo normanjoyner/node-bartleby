23bf2e50ade621d3c33ac0ee4a18ebdf759abba7
Norman Joyner <norman.joyner@gmail.com>
2013-03-10 14:02:33 -0400

    executable file to start bartleby

---
+++ application.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+// import dependencies
+var fs = require("fs");
+var args = process.argv.slice(2);
+var operations = require([__dirname, "operations"].join("/")).operations;
+
+fs.exists([process.cwd(), "node_modules"].join("/"), function(exists){
+    if(!exists)
+        fs.mkdirSync([process.cwd(), "node_modules"].join("/"));
+});
+
+fs.exists([process.env.HOME, ".bartleby"].join("/"), function(exists){
+    if(!exists)
+        fs.mkdirSync([process.env.HOME, ".bartleby"].join("/"));
+});
+
+fs.exists([process.env.HOME, ".bartleby", "tmp"].join("/"), function(exists){
+    if(!exists)
+        fs.mkdirSync([process.env.HOME, ".bartleby", "tmp"].join("/"));
+});
+
+var operation = args[0];
+var params = args.slice(1);
+
+if(operation == "install")
+    operations.install(params);
+else if(operation == "clean")
+    operations.clean();
+else
+    throw new Error("Please use either 'bartleby clean' or 'bartleby install'");
